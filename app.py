dff01d18659250d6723063e52d0641476a94348f
CaueFollador <57156557+CaueFollador@users.noreply.github.com>
2022-07-05 22:27:00 -0300

    mudando a mensagem da API

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
+    return "Hello world doesn't make you a great dev. Change my mind"
 
 if __name__ == '__main__':
     app.run()
--- test.py
+++ test.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Hello World")
+        self.assertEqual(self.result.data.decode('utf-8'), "Hello world doesn't make you a great dev. Change my mind")
